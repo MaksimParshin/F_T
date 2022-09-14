37b2f526538abfbe72b70c31ca1fe412fede9b18
MaksimParshin <mak.yp@yandex.ru>
2022-09-14 16:19:40 +0300

    create method  SortArray

--- Program.cs
+++ Program.cs
@@ -7,3 +7,21 @@
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 ["Russia", "Denmark", "Kazan"] -> []
 */
+
+void SortArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i].Length > 0 && array[i].Length <= 3)
+            System.Console.WriteLine(array[i]);
+    }
+}
+
+string[] arr1 = {"hello", "2", "world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2", "computer science"};
+string[] arr3 = {"Russia", "Denmark", "Kazan"};
+SortArray(arr1);
+Console.WriteLine();
+SortArray(arr2);
+Console.WriteLine();
+SortArray(arr3);
